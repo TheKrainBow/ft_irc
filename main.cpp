8dfafca56c6c2d860057d56da958fcd2b0f6e5e7
Marina DELWAULLE <mdelwaul@e2r9p3.clusters.42paris.fr>
2022-03-21 23:17:41 +0100

    debut de accept

--- main.cpp
+++ main.cpp
@@ -18,6 +18,24 @@ int test_args(int ac, char **av)
     }
     return (0);
 }
+int sock_bind_listen(const sockaddr_in *addr, int fd)
+{
+    if (bind(fd, (const sockaddr *)addr, sizeof(*addr)))
+        return (-1);
+    if (listen(fd, SOMAXCONN))//SOMAXCONN laisse le systeme choisir le nombre max de connections
+        return (-2);
+    return (0);
+}
+
+int sock_bind_listen(const sockaddr_in *addr)
+{
+    int fd = socket(PF_INET6, SOCK_STREAM, 0); //PF_INET6 pour IPv6, SOCK_STREAM est securise et ne fixe pas une taille de paquet
+    if (bind(fd, (const sockaddr *)addr, sizeof(*addr)))
+        return (-1);
+    if (listen(fd, SOMAXCONN))//SOMAXCONN laisse le systeme choisir le nombre max de connections
+        return (-2);
+    return (fd);
+}
 
 int initSock(sockaddr_in *addr, long port, int fd)
 {
@@ -25,10 +43,8 @@ int initSock(sockaddr_in *addr, long port, int fd)
     addr->sin_port = htonl(port);
     addr->sin_family = AF_INET6; //pour choper les IPv6
     //addr->sin_zero sert a rien, juste a s'assurer que la structure prenne 16 octets
-    if (bind(fd, reinterpret_cast<const sockaddr_in *>(addr), sizeof(*addr)))
+    if (sock_bind_listen(addr, fd))
         return (-1);
-    if (listen(fd, SOMAXCONN))//SOMAXCONN laisse le systeme choisir le nombre max de connections
-        return (-2);
     return (0);
 }
 
@@ -40,13 +56,15 @@ int main(int ac, char **av)
     sockaddr_in *addr = new sockaddr_in;
     if (initSock(addr, atol(av[1]), fd))
         return (ERR_SOCKET);
-    accept(fd, static_cast<sockaddr *> (addr), sizeof(static_cast<sockaddr> (*addr)));
-    //listen
+    socklen_t size = sizeof(*addr);
+    int portNewClient = accept(fd, (sockaddr *)addr, &size); //va bloquer jusqu'a avoir au moins une tentative de connexion
+    sockaddr_in *addrNewClient = new sockaddr_in; //TODO : il faudrait faire une liste chainee des clients a surveiller avec poll
+    sock_bind_listen(addrNewClient, portNewClient);
     while (poll() > 0)
     {
         //trucs
 
 
-        accept();
+        portNewClient = accept();
     }
 }
